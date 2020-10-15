70b7960c99012c8358d135c83090ea800eab7504
Leonardo Costa <leonardo.fc2@outlook.com>
2020-10-15 19:24:54 -0300

    feat: add trabalho1.c

---
+++ trabalho1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#define QTDE_FRASE 2
+#define TAM_FRASE 99
+
+void *ThreadFrase(void *arg) {
+    printf("%s\n", arg);
+    return NULL;
+}
+
+int main() {
+
+    char frases[QTDE_FRASE][TAM_FRASE] = {"Instituto", "Programacao"};
+    pthread_t threads[QTDE_FRASE];
+
+    int i;
+    for (i = 0; i < QTDE_FRASE; i += 1) {
+        pthread_create(&(threads[i]), NULL, ThreadFrase, frases[i]);
+    }
+
+    for (i = 0; i < QTDE_FRASE; i += 1) {
+        pthread_join(threads[i], NULL);
+    }
+    printf("Fim\n");
+    return 0;
+}
